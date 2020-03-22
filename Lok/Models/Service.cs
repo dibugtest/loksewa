0bc56bbba53f085f0e3cb088df5189d456b47b56
Rupak <Rupak@DESKTOP-BUVVMFV>
2020-03-22 11:29:27 +0545

    ServiceClassAdded

--- Lok/Models/Service.cs
+++ Lok/Models/Service.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,10 @@ namespace Lok.Models
 {
     public class Service
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
     }
 }
